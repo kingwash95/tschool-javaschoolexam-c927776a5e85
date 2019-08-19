181f4736051ce29066d4c4b86cb4b52352464bac
kingwash95 <53770054+kingwash95@users.noreply.github.com>
2019-08-19 21:20:34 +0300

    Update PyramidBuilder.java

--- tschool-javaschoolexam-c927776a5e85/src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
+++ tschool-javaschoolexam-c927776a5e85/src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
@@ -1,21 +1,30 @@
-package com.tsystems.javaschool.tasks.pyramid;
-
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 
-public class PyramidBuilder {
-
-    /**
-     * Builds a pyramid with sorted values (with minumum value at the top line and maximum at the bottom,
-     * from left to right). All vacant positions in the array are zeros.
-     *
-     * @param inputNumbers to be used in the pyramid
-     * @return 2d array with pyramid inside
-     * @throws {@link CannotBuildPyramidException} if the pyramid cannot be build with given input
-     */
-    public int[][] buildPyramid(List<Integer> inputNumbers) {
-        // TODO : Implement your solution here
-        return new int[0][0];
+public class Subsequence {
+    public static  void main (String[] args){
+        List<Object> X = Arrays.asList("A", "B", "C", "D");
+        List<Object> Y = Arrays.asList("BD", "A", "ABC", "B", "M", "D", "M", "C", "DC", "D");
+System.out.println(comparison( X,  Y));
     }
 
 
+    public static boolean comparison (List<Object> X, List<Object> Y) {
+        int XIdx = 0;
+        int YIdx = 0;
+        while (XIdx < X.size()) {
+            Object XElem = X.get(XIdx);
+            Object YElem = Y.get(YIdx);
+            while (!XElem.equals(YElem)) {
+                if (++YIdx > Y.size() - 1) {
+                    return false;
+                }
+                YElem = Y.get(YIdx);
+            }
+            XIdx++;
+        }
+        return true;
+    }
 }
