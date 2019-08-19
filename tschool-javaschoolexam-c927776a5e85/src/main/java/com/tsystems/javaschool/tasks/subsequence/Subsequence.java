a86ff50175520923b41981b3d8f9ddbbae839a83
kingwash95 <53770054+kingwash95@users.noreply.github.com>
2019-08-19 21:22:51 +0300

    Update Subsequence.java

--- tschool-javaschoolexam-c927776a5e85/src/main/java/com/tsystems/javaschool/tasks/subsequence/Subsequence.java
+++ tschool-javaschoolexam-c927776a5e85/src/main/java/com/tsystems/javaschool/tasks/subsequence/Subsequence.java
@@ -1,20 +1,30 @@
-package com.tsystems.javaschool.tasks.subsequence;
-
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Subsequence {
+    public static  void main (String[] args){
+        List<Object> X = Arrays.asList("A", "B", "C", "D");
+        List<Object> Y = Arrays.asList("BD", "A", "ABC", "B", "M", "D", "M", "C", "DC", "D");
+System.out.println(comparison( X,  Y));
+    }
 
-    /**
-     * Checks if it is possible to get a sequence which is equal to the first
-     * one by removing some elements from the second one.
-     *
-     * @param x first sequence
-     * @param y second sequence
-     * @return <code>true</code> if possible, otherwise <code>false</code>
-     */
-    @SuppressWarnings("rawtypes")
-    public boolean find(List x, List y) {
-        // TODO: Implement the logic here
-        return false;
+
+    public static boolean comparison (List<Object> X, List<Object> Y) {
+        int XIdx = 0;
+        int YIdx = 0;
+        while (XIdx < X.size()) {
+            Object XElem = X.get(XIdx);
+            Object YElem = Y.get(YIdx);
+            while (!XElem.equals(YElem)) {
+                if (++YIdx > Y.size() - 1) {
+                    return false;
+                }
+                YElem = Y.get(YIdx);
+            }
+            XIdx++;
+        }
+        return true;
     }
 }
